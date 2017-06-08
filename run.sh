4146b2c90f6cda18f3b74d4bc003c15abc63fef9
Frankblood <1062947208@qq.com>
2017-06-08 10:19:58 -0700

    script for run

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 nohup python train.py 3 > ./log/cnn_rnn_tmp &
+# CUDA_VISIBLE_DEVICES=0 nohup python train.py 0 > ./log/basic_baseline &
+# CUDA_VISIBLE_DEVICES=0 nohup python train.py 1 > ./log/basic_attention &
+# CUDA_VISIBLE_DEVICES=0 nohup python train.py 2 > ./log/cnn_rnn &
+# CUDA_VISIBLE_DEVICES=0 nohup python train.py 3 > ./log/cnn_rnn_tmp &
+CUDA_VISIBLE_DEVICES=0 nohup python train.py 4 > ./log/basic_cnn &
